1b28f780a78ef37e2b2bae197940330c9bda1ff2
guoquanhao <cybergodhao@gmail.com>
2026-04-08 11:39:19 +0800

    add qianfan-ocr_custom_training

--- qianfan-ocr/qianfan-ocr_custom_training/qianfan-ocr_grpo/qianfanocr_table_reward_plugin_html.py
+++ qianfan-ocr/qianfan-ocr_custom_training/qianfan-ocr_grpo/qianfanocr_table_reward_plugin_html.py
@@ -1,12 +1,17 @@
-"""
-!/usr/bin/python3.10
--*- encoding: utf-8 -*-
-@Time    :  14:08
-@Author  : GuoQuanhao
-@Email   : guoquanhao@baidu.com
-@File    : reward.py
-@Software: PyCharm
-"""
+# Copyright (c) 2024 Baidu, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 import distance
 import numpy as np
